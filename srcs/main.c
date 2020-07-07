0ce0e0356a8aa109e7f317a968acb27284836cf4
Remco Pet <rpet@f1r4s10.codam.nl>
2020-07-07 11:59:45 +0200

    main.c moet weg

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thvan-de <thvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/29 11:49:44 by thvan-de      #+#    #+#                 */
-/*   Updated: 2020/06/29 14:10:54 by thvan-de      ########   odam.nl         */
+/*   Updated: 2020/07/01 10:06:12 by rpet          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,78 +23,48 @@ void	ft_error(char *str)
 	exit(1);
 }
 
-void 	ft_create_token()
+void	create_token(char *str)
 {
 	
-	ft_lstadd_back()
 }
 
-void    ft_found_quot(char **line, int  quote)
+void    ft_parse_line(char *line)
 {
-	int i;
+	int			quote;
+	int			i;
+	int			token_len;
 
+	quote = 0; // 0 is geen 1 is single 2 is double
 	i = 0;
-	if (line[i] -1 == '\\' && quote ==  '0')
-		ft_error("Multiline command not handled by our program\n");
-	else if (line[i] -1 == '\\' && quote ==  '\"')
-		
-		
-	while(line[i] != quote)
-	{
-
-	}
-	ft_create_token()
-}
-
-void 	ft_new_command()
-{
-
-	ft_create_token()
-}
-
-void 	ft_check_redirect(line)
-{
-	
-	ft_create_token()
-}
-
-void    ft_parse_line(char **line)
-{
-	char		quote;
-
-	quote = '0'; // 0 is geen 1 is single 2 is double
-    while(*line != '\0')
+    token_len = 0;
+	if (line[i] == '\\')
+		ft_error("Command not found");
+	while (line[i] != '\0')
     {
-        if (*line == '\"' && quote == '0')
+		if ((line[i] == '\'' || line[i] == '\"') && quote == 0)
 		{
-			quote = '\"';
-            ft_found_quot(line, quote);
+			if (line[i - 1] == '\\')
+
+			}
+			quote = 1;
 		}
-        if (*line == '\'' && quote == '0')
-		{
-			quote = '\'';
-            ft_found_quot(line, quote);
-		}
-		if (*line == ';')
-			ft_new_command(line);
-		ft_check_redirect(line);
+		i++;
     }
 }
 
-int main(int argc, char **argv, char **env)
+int		main(int argc, char **argv, char **env)
 {
     int     i;
     char    *line;
     int     fd;
 
     i = 1;
-
     while (i)
     {
         i = get_next_line(0, &line);
         if (i == -1)
             ft_error("Something went wrong reading the line\n");
-        ft_parse_line(&line);
+        ft_parse_line(line);
     }
-
+	return (0);
 }
